14650d214bf2a0e7e876bfad95481ef243ae3500
Konstantin <crahercenter@gmail.com>
2025-02-24 04:28:46 +0000

    Выполнено задание 6-2: созданы модели и миграции для Note и Todo

--- note-app/database/seeders/DatabaseSeeder.php
+++ note-app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Todo;
+use App\Models\Note;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +19,16 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
+            'id' => 1,
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password' => bcrypt('pass123.')
         ]);
+
+        // Создаем 100 заметок для теста
+        Note::factory(100)->create();
+
+        // Добавьте сюда новый код, если нужно
+        Todo::factory(10)->create();
     }
 }
